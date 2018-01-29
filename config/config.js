23d5e639d8b3d91d8f17b1eefc30cdf7c0e5aa6d
ptgetter <ptgetter@nate.com>
2018-01-29 15:14:56 +0900

    오류 수정

--- config/config.js
+++ config/config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    imageServerUrl: 'http://192.168.1.201:8080'
-  };
+  imageServerUrl: 'http://192.168.1.201:8080',
+  ftpConfig: {
+    host: '192.168.1.201',
+    port: 21,
+    user: 'poptokimage',
+    password: 'bigdata2017'
+  },
+  logDir: '/home/datamaster/workspace/log/poptokapi'
+};
