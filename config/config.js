8302aa21a7e0c9617b449694979a44b2090cf72a
ptgetter <ptgetter@nate.com>
2018-01-31 14:49:09 +0900

    이미지 서버 주소 변경

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  imageServerUrl: 'http://192.168.1.201:8080',
+  imageServerUrl: 'http://bitbigdata.iptime.org:8080',
   redisConfig: {
     host :'192.168.1.207',
     port : 6379
